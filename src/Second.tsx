3930c0315730b77900dc608c9a96e2aecdcd9c20
Mohammed Berrada <39965111+tokismos@users.noreply.github.com>
2023-05-14 21:14:54 -0400

    Chore: Import with absolute paths

---
+++ src/Second.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+const SecondScreen = (): JSX.Element => {
+	return (
+		<View>
+			<Text>Second screen</Text>
+		</View>
+	);
+};
+
+export default SecondScreen;
--- src/components/FirstScreen.tsx
+++ src/components/FirstScreen.tsx
@@ -1,10 +1,12 @@
 import { View, Text } from "react-native";
 import React from "react";
+import SecondScreen from "src/Second";
 
 const FirstScreen = (): JSX.Element => {
 	return (
 		<View>
 			<Text>FirstScreen</Text>
+			<SecondScreen />
 		</View>
 	);
 };
